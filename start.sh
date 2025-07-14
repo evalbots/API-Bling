b5bf4433ca012c3dfc68e91bfce7d0ab5a230dcf
evalbots <terereviceverso@gmail.com>
2025-07-14 15:50:23 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Instala o Chromium e dependências
 playwright install --with-deps chromium
+
+# Inicia o Uvicorn, usando a porta que o Railway fornece
 uvicorn bling_redirect_api:app --host 0.0.0.0 --port ${PORT:-8000}
