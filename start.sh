f7fc346d0a255591ab362ca7c17ed0a6abe82ea5
evalbots <terereviceverso@gmail.com>
2025-07-14 16:35:13 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-# Instala o Chromium e dependências
+#!/bin/bash
 playwright install --with-deps chromium
-
-# Inicia o Uvicorn, usando a porta que o Railway fornece
 uvicorn bling_redirect_api:app --host 0.0.0.0 --port ${PORT:-8000}
