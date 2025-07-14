1eb475023f971e6cb768a9f9de8d961b107c594f
evalbots <terereviceverso@gmail.com>
2025-07-14 17:01:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-playwright install --with-deps chromium
+echo "Starting Container"
+
+# Instala o Chromium com dependências via Playwright
+npx playwright install --with-deps chromium
+
+# Inicia o servidor com Uvicorn na porta definida pela variável de ambiente PORT (padrão 8000)
 uvicorn bling_redirect_api:app --host 0.0.0.0 --port ${PORT:-8000}
